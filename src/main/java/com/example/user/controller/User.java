6530fe8114b0d4925a1c9db0793390e56c7bdd04
ssulochanaherath <ssulochanaherath@gmail.com>
2025-04-04 09:18:08 +0530

    save user created

--- src/main/java/com/example/user/controller/User.java
+++ src/main/java/com/example/user/controller/User.java
@@ -1,4 +1,20 @@
 package com.example.user.controller;
 
+import com.example.user.service.Weavy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/users")
 public class User {
+    @Autowired
+    private Weavy weavyService;
+
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody Map<String, String> user) {
+        return ResponseEntity.ok(weavyService.createUser(user));
+    }
 }
--- src/main/java/com/example/user/service/Weavy.java
+++ src/main/java/com/example/user/service/Weavy.java
@@ -1,4 +1,24 @@
 package com.example.user.service;
 
+import com.google.gson.Gson;
+import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
 public class Weavy {
+    @Value("${weavy.api.url}")
+    private String apiUrl;
+
+    @Value("${weavy.token}")
+    private String bearerToken;
+
+    private final OkHttpClient client = new OkHttpClient();
+    private final Gson gson = new Gson();
+
+    public String createUser(Map<String, String> userData) {
+        return sendRequest("/users", "POST", gson.toJson(userData));
+    }
 }
